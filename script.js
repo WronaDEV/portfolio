aac34715c1255cf71201069a08b22b39956390e5
Wrona <87375046+WronaDEV@users.noreply.github.com>
2024-01-21 10:55:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttonsMenu = document.getElementById("buttons-menu");
+const buttonsMenuOverlay = document.getElementById("buttons-menu-overlay");
+const buttonsMenuButton = document.getElementById("right-button");
+
+function menuButton() {
+    buttonsMenu.style.display = "block";
+    buttonsMenuOverlay.style.display = "block";
+}
