f54d991d3341f1d3f9a9784b43818b52281d1188
Wrona <87375046+WronaDEV@users.noreply.github.com>
2024-01-21 11:52:36 +0100

    Add files via upload

--- VideoAgencySite/script.js
+++ VideoAgencySite/script.js
@@ -2,14 +2,18 @@ let show = 'show';
 let unshow = 'unshow';
 const buttonsMenu = document.getElementById("buttons-menu");
 const buttonsMenuOverlay = document.getElementById("buttons-menu-overlay");
-const buttonsMenuButton = document.getElementById("right-button");
+const buttonsMenuButton = document.getElementById("right-button-in");
 
 function menuButton() {
     if (~buttonsMenu.className.indexOf(unshow)) {
         buttonsMenu.className = buttonsMenu.className.replace(unshow, show);
         buttonsMenuOverlay.className = "show";
+        buttonsMenuButton.className = "fa-solid fa-xmark fa-2xl";
+        buttonsMenuButton.style.color = "#fff";
     } else {
         buttonsMenu.className = buttonsMenu.className.replace(show, unshow);
         buttonsMenuOverlay.className = "unshow";
+        buttonsMenuButton.className = "fa-solid fa-bars fa-2xl";
+        buttonsMenuButton.style.color = "black";
     }
 }
